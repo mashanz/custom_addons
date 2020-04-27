e16e0e0a8513018364224efdaa8121f5cdfda6d7
Hanjara CA (HANS) <mangatkk@gmail.com>
2020-04-27 15:02:57 +0700

    add modul hospital

---
+++ mod_hospital/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Hospital Management',
+    'version': '0.1',
+    'category': 'Extra Tools',
+    'summary': 'Module for managing the Hospital',
+    'sequence': '10',
+    'license': 'AGPL-3',
+    'author': 'Hanjara',
+    'maintainer': 'Hanjara',
+    'website': 'www.officeboytokosebelah.com',
+    'depends': [],
+    'demo': [],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
