c477cff07b1a34a3545d155a3b10452cd8c4c0f0
Hanjara CA (HANS) <mangatkk@gmail.com>
2020-04-27 15:16:48 +0700

    add patient model with mandatory fields

---
+++ mod_hospital/patient.py
@@ -0,0 +1,10 @@
+from odoo import models, fields
+
+class HospitalPatient(models.Model):
+    _name = 'hospital.patient'
+    _description = 'Patient Records'
+
+    patient_name = fields.Char(string='Name', required=True)
+    patient_age = fields.Integer('Age')
+    notes = fields.Text(string="Notes")
+    image = fields.Binary(string="Image")
