ed8ef443f75981a9f6b870fdb11705a10c2b7f76
Hanjara CA (HANS) <mangatkk@gmail.com>
2020-04-27 07:34:15 +0700

    add icon

--- open_academy/__manifest__.py
+++ open_academy/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "open_academy",
+    'name': "Open Academy",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -10,8 +10,8 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Mashanz",
+    'website': "http://www.officeboytokosebelah.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
