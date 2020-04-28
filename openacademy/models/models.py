0775ca0395447f2ef9f3ed902208c0ea30e9e23e
Hanjara CA (HANS) <mangatkk@gmail.com>
2020-04-28 13:56:08 +0700

    Create Model and Data

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
+class Course(models.Model):
+    _name = 'openacademy.course'
+    _description = "Open Academy Courses"
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
 
 # class openacademy(models.Model):
 #     _name = 'openacademy.openacademy'
